ac2bf3999fd72751e79168d63864c8ae2180a56f
Paul Cristian <paul931224@gmail.com>
2022-05-16 13:23:07 +0200

    Iterating further on the idea.
    We need to make our first plain item which can be positioned. For this we may have to start with the sidebar first.

--- source-code/wizard/editor/core.cljs
+++ source-code/wizard/editor/core.cljs
@@ -2,10 +2,14 @@
  (:require  [re-frame.core :refer [dispatch reg-event-db reg-event-fx reg-sub]]))
 
 
-(defn editor-particle [column-index row-index]
- [:div {:style {:width "9px" 
-                :height "9px" 
-                :border "1px solid white"}}])
+(defn editor-particle [col-index row-index]
+ [:div 
+  {:class ["editor-particle"
+           (str "col-" col-index)
+           (str "row-" row-index)]
+   :style {:flex-grow 1
+           :height "100%" 
+           :border "1px solid white"}}])
                 
 
 (defn editor-wrapper [content]
@@ -13,12 +17,18 @@
   [:div#editor {:style {:max-width "1200px" :display :flex :flex-wrap :wrap}}
    content]])
 
+(defn row-wrapper [content]
+  [:div {:style {:width "100%" :display :flex 
+                 :height "20px"}}
+    content])    
+ 
 (defn view [] 
- (let [width-particles   (range 120)
+ (let [width-particles   (range 60)
        height-particles  (range 240)]
    [editor-wrapper 
      (map 
-      (fn [column-index]
-        (map (fn [row-index] [editor-particle column-index row-index])
-             height-particles))
-      width-particles)]))
+      (fn [col-index]
+        [row-wrapper
+         (map (fn [row-index] [editor-particle col-index row-index])
+              width-particles)])
+      height-particles)]))
