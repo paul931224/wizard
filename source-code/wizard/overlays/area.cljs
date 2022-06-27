974c201c01c65a2d1c3182599e1d654009b4b906
Paul Cristian <paul931224@gmail.com>
2022-06-27 02:30:52 +0200

    Overlay on top of overlay. Won't work, but that's a start

--- source-code/wizard/overlays/area.cljs
+++ source-code/wizard/overlays/area.cljs
@@ -30,7 +30,9 @@
  (let [numbers (range how-many)]
   (vec (map utils/number-to-letter numbers))))
 
-(defn randomize-rgb []
+
+
+(defn randomize-rgb [a]
   (let [value-range (range 256)
         r (rand-nth value-range)
         g (rand-nth value-range)
@@ -39,12 +41,35 @@
         rgba-code (clojure.string/join ", " [r g b a])]
     (str "rgba(" rgba-code ")")))
 
-(defn grid-item [item grid-data]
+(def random-colors 
+ (mapv randomize-rgb (range 26)))
+
+
+(defn grid-item [index item]
+ [:div {:style    {:background "rgba(0,0,0,0.3)"
+                   :display :flex
+                   :justify-content :center
+                   :align-items :center
+                   :color "#DDD"
+                   :height "100%"
+                   :width "100%"
+                   :position :relative}}
+                    
+       [:div {:style {:background "#333"
+                      :height "30px"
+                      :width "30px"
+                      :display :flex 
+                      :justify-content :center
+                      :align-items :center
+                      :border-radius "15px"}}
+        (str item)]])
+
+(defn area-item [item]
   (let [letter  (fn [] (utils/number-to-letter (:position (second item))))
         active  (fn [] @(subscribe [:db/get [:overlay :active]])) 
         active? (fn [] (= (letter) (active)))]
    [:div {:on-click (fn [e] (dispatch [:db/set [:overlay :active] (letter)]))
-          :style    {:background (randomize-rgb)
+          :style    {:background (get random-colors (:position (second item)))
                      :display :flex
                      :justify-content :center
                      :align-items :center
@@ -67,8 +92,39 @@
           [expand-horizontal-indicator]
           [expand-vertical-indicator]])]))
    
+(defn grid-layer [value-path all-area-cells]
+ [overlay-wrapper/view
+  [:div#area-overlay
+   {:style {:height "100%"
+            :width "100%"
+            :backdrop-filter "blur(1px)"
+            :position :absolute
+            :pointer-events :auto
+            :left 0
+            :z-index 2}}
+   [grid/grid-wrapper
+    (map-indexed (fn [index item] [grid-item index item])
+                 all-area-cells)
+    (vector
+     (last value-path)
+     @(subscribe [:db/get value-path]))]]])
 
-
+(defn area-layer [value-path components grid-data]
+ [overlay-wrapper/view
+  [:div#area-overlay
+   {:style {:height "100%"
+            :width "100%"
+            :backdrop-filter "blur(1px)"
+            :position :absolute
+            :pointer-events :auto
+            :left 0
+            :z-index 2}}
+   [grid/grid-wrapper
+    (map-indexed (fn [index item] [area-item item grid-data])
+                 components)
+    (vector
+     (last value-path)
+     @(subscribe [:db/get value-path]))]]])
 
 (defn view []
   (let [overlay (subscribe [:db/get [:editor :overlay :type]])
@@ -78,21 +134,11 @@
         components            (fn [] @(subscribe [:db/get (components-value-path)]))
         col-count    (fn [] (count (:cols (grid-data))))
         row-count    (fn [] (count (:rows (grid-data))))
+        all-area-cells    (fn [] (flatten (:areas (grid-data))))
         items-count  (fn [] (count (components)))
         abc-matrix   (fn [] (generate-abc-matrix (items-count)))]
      (if (= :area @overlay)
-      [overlay-wrapper/view
-          [:div#area-overlay
-           {:style {:height "100%"
-                     :width "100%"
-                     :backdrop-filter "blur(1px)"
-                     :position :absolute
-                     :pointer-events :auto
-                     :left 0
-                     :z-index 2}}                     
-           [grid/grid-wrapper
-            (map-indexed (fn [index item] [grid-item item (grid-data)])
-                      (components))
-            (vector
-             (last (value-path))
-             @(subscribe [:db/get (value-path)]))]]])))
+      [:<>         
+        [grid-layer (value-path) (all-area-cells)]
+        [area-layer (value-path) (components) (grid-data)]])))
+        
