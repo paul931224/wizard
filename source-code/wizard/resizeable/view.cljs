af629c98fc042a15359751aedddeded7f4dd438b
Paul Cristian <paul931224@gmail.com>
2022-06-27 22:17:24 +0200

    Right resizing works now.

--- source-code/wizard/resizeable/view.cljs
+++ source-code/wizard/resizeable/view.cljs
@@ -11,52 +11,26 @@
 
 (def dnd-context (r/adapt-react-class DndContext))
 
-(defn resizeable-item [listeners label component id]
+(def resize-atom (r/atom {:top 0
+                          :left 0
+                          :height 50
+                          :width 100}))
+
+(defn resizeable-item [label component id]
   (let [style {:style {:font-weight :bold
-                       :padding "10px"
-                       :cursor :resize                       
-                       :margin-bottom "10px"}}]
+                       
+                       :cursor :resize                                             
+                       :height "100%"}}]
     [:div.resizeable-area 
-     (merge listeners style
+     (merge style
             {:on-click #(dispatch [:db/set [:editor :toolbar :active] id])})
-     component]))
+     [:div {:style {:padding "10px"}}
+      component]]))
 
 
+                        
+                        
 
-(defn resize-s []
-  [:div {:style {:position :absolute
-                 :width "100%"
-                 :background :red
-                 :height "5px"
-                 :cursor "s-resize"
-                 :bottom 0
-                 :left 0}}])
-
-(defn resize-n []
-  [:div {:style {:position :absolute
-                 :width "100%"
-                 :background :red
-                 :height "5px"
-                 :cursor "n-resize"
-                 :top 0
-                 :left 0}}])
-
-(defn resize-e []
-  [:div {:style {:position :absolute
-                 :width "5px"
-                 :background :red
-                  :height "100%"
-                 :cursor "e-resize"
-                 :bottom 0
-                 :right 0}}])                                  
-
-(defn resize-w []
- [:div {:style {:position :absolute
-                :width "5px"
-                :background :red 
-                :height "100%"
-                :cursor "w-resize"
-                :bottom 0}}])
 
 (defn draggable [props]
   (let [id                    (:id props)
@@ -68,15 +42,14 @@
                 setNodeRef]}  use-draggable]
        
        
-    [:div (merge {:style {:position :relative}
+    [:div (merge {:style {:position :relative
+                          :height (str (:height @resize-atom) "px")
+                          :width  (str (:width  @resize-atom)  "px")}
                   :class ["area"]                          
                   :ref (js->clj setNodeRef)}                 
-                 attributes)
-     [resizeable-item listeners label component id]
-     [resize-w]
-     [resize-e]
-     [resize-n]
-     [resize-s]]))
+                 attributes
+                 listeners)
+     [resizeable-item label component id]]))
      
      
 
@@ -90,16 +63,36 @@
 
 (defn handle-drag-end [event]
   (let [{:keys [active over]} (utils/to-clj-map event)
-        id      (:id active)]
-    (dispatch [:db/set [:editor :toolbar  :dragged] nil])
-    (dispatch [:db/set [:editor :toolbar  :active] id])))
+        id      (:id active)] 
+    (reset! resize-atom (dissoc @resize-atom :bottom :top :right :left))
+    (dispatch [:db/set [:area-editor :dragged] nil])
+    (dispatch [:db/set [:area-editor :active] id])))
 
 (defn handle-drag-move [event]
   (let [{:keys [active over]} (utils/to-clj-map event)
         id      (:id active)
         new-pos (-> active :rect :current :translated)
-        top-and-left (select-keys new-pos [:top :left])]
-    (dispatch [:db/set [:editor :toolbars id] top-and-left])))
+                                           
+        old-directions (select-keys @resize-atom [:bottom :top
+                                                  :left   :right])                            
+        new-directions (select-keys new-pos [:bottom :top
+                                             :left   :right])
+        left-delta     (- (:left  old-directions) (:left  new-directions))]           
+    
+    (if (contains? old-directions :right)
+      (do 
+        (.log js/console (str "hello: ")
+                      "old: "  (int (:left old-directions)) 
+                      " - " 
+                      "new: "  (int (:left new-directions)) 
+                      " - " 
+                      "delta:" (int left-delta))                                        
+        (reset! resize-atom (merge @resize-atom 
+                              {:width (- (:width @resize-atom) left-delta)
+                               :left (- (:left @resize-atom) left-delta)})))                                                                                          
+      (reset! resize-atom (merge @resize-atom new-directions)))))
+                                       
+   
 
 (defn toolbar [config]
   [:f> draggable config])
@@ -111,8 +104,8 @@
 
     [dnd-context {:onDragStart   handle-drag-start
                   :onDragMove     handle-drag-move
-                  :onDragEnd      handle-drag-end
-                  :modifiers      [restrictToWindowEdges]}
+                  :onDragEnd      handle-drag-end}
+                  ;:modifiers      [restrictToWindowEdges]}
      content]])
 
 (defn area []
